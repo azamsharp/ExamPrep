a35e96c6974342a376be604565de9c1d4622df3f
Mohammad Azam <azamsharp@gmail.com>
2023-09-14 22:24:01 -0500

    registration

--- ExamPrepServer/controllers/userController.js
+++ ExamPrepServer/controllers/userController.js
@@ -1,26 +1,53 @@
 
-const { body, validationResult } = require('express-validator') 
+const { body, validationResult } = require('express-validator')
+const models = require('../models')
 
 exports.register = async (req, res) => {
-    
+
+    const { email, password } = req.body 
+
     const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() })
-        return 
+    if (!errors.isEmpty()) {
+        res.status(400).json({ error: errors.array().map(error => error.msg).join(' ') })
+        return
     }
 
-    // register the user 
+    // check if the user is already registered 
+    const user = await models.User.findOne({
+        where: {
+            email: email
+        }
+    })
 
-    res.send('OK')
+    if (user) {
+        // already exists 
+        res.status(400).json({ error: 'Email is already registered.' })
+        return
+    }
+
+    try {
+        // register new user 
+        const newUser = await models.User.create({
+            email: email,
+            password: password // need to bcrypt the password 
+        })
+
+        // 201 for created and send the new user back 
+        res.status(201).json({success: true})
+
+    } catch(error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
 }
 
 exports.validate = (method) => {
 
-    switch(method) {
+    switch (method) {
         case 'register': {
             return [
-                body('email', 'Email cannot be empty.').exists().isEmail(), 
-                body('password', 'Password must be at least 6 characters long').exists().isLength({ min: 6})
+                body('email', 'Email cannot be empty.').exists().isEmail(),
+                body('password', 'Password must be at least 6 characters long').exists().isLength({ min: 6 })
             ]
         }
     }
