7a14ac34ab00d6110153e7e2b7b75b364a6c58b2
Mohammad Azam <azamsharp@gmail.com>
2023-09-18 16:45:02 -0500

    authentication middleware

--- ExamPrepServer/routes/index.js
+++ ExamPrepServer/routes/index.js
@@ -3,9 +3,14 @@ const express = require('express')
 const router = express.Router() 
 const { body, validationResult } = require('express-validator') 
 const authController = require('../controllers/authController') 
+const { authenticate } = require('../middlewares/authMiddleware')
 
 router.post('/register', authController.validate('register'), authController.register)
 router.post('/login', authController.validate('login'), authController.login)
 router.get('/roles', authController.getRoles)
+router.get('/secure', authenticate, (req, res) => {
+    console.log(req.userId)
+    res.json({userId: req.userId})
+})
 
 module.exports = router 
